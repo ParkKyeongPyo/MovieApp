667323eff590b73306de8babddb4407cb8255eb5
Park Kyeong Pyo <as8798as@gmail.com>
2021-10-12 20:04:01 +0900

    무비앱 첫 푸쉬

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = '4f466dc2ea0ee0f8fee82c14a8a15354'
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
